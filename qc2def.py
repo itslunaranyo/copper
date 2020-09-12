1252c00f6b5b528b611a5ab810673b436c8114d1
Matthew Breit <itslunaranyo@gmail.com>
2020-09-12 13:17:40 -0700

    Initial commit: v 1.11 with some changes for 1.12 already in

---
+++ qc2def.py
@@ -0,0 +1,71 @@
+import os
+
+# gtkradiant doesn't add skill flags by default and I'd rather keep the source 
+# clean by adding them here instead, because what else is python for?
+def addSkillFlags(deftxt):
+	deflines = deftxt.split("\n")
+	init = deflines[0]
+	initbits = init.split()
+	
+	cname = initbits[1]
+	if (cname == "func_group" or cname.startswith("func_detail") or cname == "worldspawn" or cname == "misc_external_map"):
+		return deftxt
+		
+	if (initbits[5][0] == '('):
+		# point ent
+		while (len(initbits) < 19):
+			initbits.append("?")
+	else:
+		# brush ent
+		while (len(initbits) < 14):
+			initbits.append("?")
+	initbits += ["NotEasy", "NotNormal", "NotHard", "NotDeathmatch", "CoopOnly", "NotCoop"]
+	
+	deflines[0] = " ".join(initbits)
+	deftxt = "\n".join(deflines)
+	return deftxt
+
+def getQuakeds(qc):
+	deftxt = ""
+	defOut = ""
+	numDefs = 0
+	start = 0
+	end = 0
+	
+	while 1:
+		start = qc.find("/*QUAKED",start)
+		if start == -1:
+			return defOut, numDefs
+		#print( "adding ", qc[start + 9 : qc.find(" ",start + 9)] )
+		end = qc.find("*/",start) + 2
+		deftxt = addSkillFlags(qc[start : end])
+		defOut += "\n" + deftxt + "\n"
+		numDefs += 1
+		start = end
+		
+
+def go():
+	cwd = os.path.dirname(os.path.realpath(__file__))
+	qcDir = cwd + "\\"
+	defDir = cwd + "\\..\\"
+
+	print("Scanning *.qc for def comments...")
+	defOut = ""
+	numDefs = 0
+	
+	for qcfn in os.listdir(qcDir):
+		if not qcfn.endswith(".qc"):
+			continue
+		with open(qcDir+qcfn, "r") as qcfile:
+			qc = qcfile.read()
+		defs, n = getQuakeds(qc)
+		defOut += defs
+		numDefs += n
+	
+	dfn = defDir + "copper.def"
+	with open(dfn, "w") as df:
+		df.write(defOut)
+	print("Completed, found",numDefs)
+
+if __name__ == "__main__":
+	go()
---
+++ qc2fgd.py
@@ -0,0 +1,76 @@
+import os, datetime
+
+# TODO: put all the baseclasses first
+
+def getQuakeds(qc):
+	defOut = ""
+	numDefs = 0
+	start = 0
+	end = 0
+	
+	while 1:
+		start = qc.find("/*FGD",start)
+		if start == -1:
+			return defOut, numDefs
+		#print( "adding ", qc[start + 9 : qc.find(" ",start + 9)] )
+		end = qc.find("*/",start)
+		defOut += qc[start + 5 : end].strip() + "\n"
+		numDefs += 1
+		start = end
+
+def getQCs():
+	# load every file listed in progs.src except the header
+	with open("progs.src",'r') as progsfile:
+		progslines = list(progsfile)[1:] # first line is output filename, skip it
+
+	qcfiles = []
+	for line in progslines:
+		qcfilename = line.partition("//")[0].strip()
+		if (len(qcfilename) < 4):
+			continue
+		if qcfilename[-3:] != ".qc":
+			continue
+		qcfiles.append(qcfilename)
+	return qcfiles
+
+
+def go():
+	cwd = os.path.dirname(os.path.realpath(__file__))
+	qcDir = cwd + "\\"
+	fgdDir = cwd + "\\..\\"
+
+	print("Scanning *.qc for FGD comments...")
+	defOut = """// Copper Quake game definition file (.fgd)
+// for Worldcraft 1.6 and above
+
+// If something is wrong with this file, check
+//   >  http://lunaran.com/files/copper.fgd  <
+// for the very latest version before anything else
+
+// Based heavily on quake.fgd by autolycus/czg/et al
+// Generated from Copper QuakeC source comments on """
+	numDefs = 0
+	defOut += datetime.datetime.now().strftime("%m.%d.%Y")
+	defOut += "\n"
+	
+	# TODO: open quakec in progs.src order for better fgd order control
+	qcfiles = getQCs()
+	for qcfn in qcfiles:
+		if not qcfn.endswith(".qc"):
+			continue
+		with open(qcDir+qcfn, "r") as qcfile:
+			qc = qcfile.read()
+		defs, n = getQuakeds(qc)
+		if (n):
+			defOut += "\n//\n// " + qcfn + "\n//\n"
+			defOut += defs
+			numDefs += n
+			defOut += "\n"
+	
+	dfn = fgdDir + "copper.fgd"
+	with open(dfn, "w") as df:
+		df.write(defOut)
+	print("Completed, found",numDefs)
+
+if __name__ == "__main__":
+	go()
